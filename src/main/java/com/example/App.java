8ab0181cb281ca3cec574dd1eed0ade1561d83f2
Gurukiran-H-S <gurukiranhs9900@gmail.com>
2026-06-16 21:23:10 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! I am gurukiran i would like to do a project based on devops engineering." );
+    }
+}
